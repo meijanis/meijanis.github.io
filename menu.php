39a8a896c7542d18d637b4e3d95bb3c5ed652699
meijanis <61264937+meijanis@users.noreply.github.com>
2020-04-10 22:00:42 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W#c//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1_transitionl.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content_Type" content="text/html; charset=utf-8" />
+<title>Modularitas</title>
+</head>
+
+<body>
+<a href="index.php">Home</a>  |
+<a href="about.php">About Us</a>  |
+<a href="link.php">Links</a>  |
+<a href="contact.php">Contact</a>  |
+</body>
+</html>
